b46443413baed0dca979cbac11905c271652c87a
nathanzagrovic <138458652+nathanzagrovic@users.noreply.github.com>
2025-05-09 08:40:02 +0100

    Update SmsAuthenticationDriver.php

--- src/Drivers/SmsAuthenticationDriver.php
+++ src/Drivers/SmsAuthenticationDriver.php
@@ -18,7 +18,7 @@ class SmsAuthenticationDriver extends AuthenticationDriver
 
         try {
             $address = $customer->addresses->first();
-            $number = $address->mobile ?: $address->phone;
+            $number = $customer->customer_2fa_telephone_number ?: $address->mobile;
 
             $message = str_replace('{{ code }}', $code, setting('customer-2fa.sms-message'));
 
