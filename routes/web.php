3c813d511a951cc0a45f28098b67c62c11d03921
nathanzagrovic <138458652+nathanzagrovic@users.noreply.github.com>
2025-05-12 13:32:09 +0100

    Update route to L6 friendly

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Techquity\AeroCustomer2Fa\Http\Controllers\Customer2faController;
 
-Route::controller(Customer2faController::class)
-    ->prefix('/customer-2fa')
-    ->name('customer-2fa.')
-    ->group(function () {
-        Route::post('/update', 'update')->name('update');
-    });
+Route::post('customer-2fa/update', 'Customer2faController@update')->prefix('/customer-2fa')->name('customer-2fa.update');
